69d55fe02ef29aafb6c3ea938a5eb05cb67ec269
aljohn <johnnjill3@gmail.com>
2020-02-04 07:53:54 +0800

    1.0.4

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const joiAccountSchema = require('./validations/account')
+const Joi = require('@hapi/joi')
+
+const joiAccountSchema = {
+    email:
+        Joi.email().required().min(5).max(50),
+    password:
+        Joi.string().required().min(5).max(1024),
+    name:
+        Joi.string().required().min(2).max(100),
+    bio:
+        Joi.string().max(100)
+}
 
 module.exports.joiAccountSchema = joiAccountSchema
